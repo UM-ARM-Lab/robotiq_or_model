78da1c825441c5cb98aa5856901def8d0a028c06
Jon Toto <jontoto@umich.edu>
2017-02-21 16:44:29 -0500

    updates

--- rob_s_open.py
+++ rob_s_open.py
@@ -3,7 +3,33 @@
 import openravepy
 import IPython
 
-if __name__ == "__main__":
+import rospy
+from robotiq_s_model_control.msg import _SModel_robot_input  as inputMsg
+class Robotiq_gripper(object):
+    def __init__(self):
+        self.A = Finger()    
+        self.B = Finger()
+        self.C = Finger()
+
+    
+class Finger():
+    def __init__(self):
+        self.status = [(0,0,0,0,0,0)]*3
+        self.joints = [(0,0,0)]*3
+        self.g = [0]*3
+        self.max_angle = (70, 90, 43)
+        self.min_3 = -55
+
+    def update_individual(index, new_g):
+        pass
+
+    def update_status():
+        pass
+
+    def update_joints(status):
+
+
+def gripper_model():
     env = openravepy.Environment()
     env.Load("env.xml")
     env.SetViewer('qtcoin')
@@ -11,4 +37,11 @@ if __name__ == "__main__":
     viewer.SetBkgndColor([.8, .85, .9])  # RGB tuple
     robot = env.GetRobots()[0]
 
-    IPython.embed()
+    gripper = Robotiq_gripper()
+    rospy.init_node('GripperDisplay')
+    rospy.Subscriber("/gripper_server/input", inputMsg.SModel_robot_input, gripper.update_joints)
+    rospy.spin()
+
+if __name__ == "__main__":
+    gripper_model()
+    
