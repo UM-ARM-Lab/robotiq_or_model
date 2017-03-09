909fa502ca0cd26ab6bc11a31e3bcd51531c2033
Jon Toto <jontoto@umich.edu>
2017-03-09 15:27:07 -0500

    added two manipulators, working on fk

--- rob_s_open.py
+++ rob_s_open.py
@@ -4,32 +4,34 @@ import openravepy
 import IPython
 import numpy as np
 import rospy
+import math
+import itertools
 # from robotiq_s_model_control.msg import _SModel_robot_input  as inputMsg
+class Test:
+    def __init__(self, g):
+        self.gPOA = g
+        self.gPOB = g
+        self.gPOC = g
+
+def Deg2Rad(angles):
+    rads = []
+    for angle in angles:
+        rads.append(angle*math.pi/180)
+    # print rads
+    return rads
 
 
-class Robotiq_gripper(object):
-    def __init__(self, robot):
-        self.gripper = robot
-        self.A = Finger()    
-        self.B = Finger()
-        self.C = Finger()
-
-    def update_fingers(self, status):
-        self.A.no_obj_update(status.gPOA)
-        self.B.no_obj_update(status.gPOB)
-        self.C.no_obj_update(status.gPOC)
     
 class Finger():
     def __init__(self):
-        self.phase = 1
-        self.state = [0,0,0,0,0,0]
-        self.joints = (0,0,0)
+        # self.phase = 1
+        # self.state = [0,0,0,0,0,0]
+        self.joints = [0.0,0.0,0.0]
         self.g = 0
-        self.max_angle = (70, 90, 43)
-        self.min_3 = -55
+        self.max_angle = (70.0, 90.0, 43.0)
+        self.min_3 = -55.0
+        self.manip
 
-        self.m1 = 70/140
-        self.m2 = 90/100
         
     def m3(self):
         return -55 + (43 + 55)/(255 - self.g)
@@ -54,31 +56,36 @@ class Finger():
 
     #function describes joint angles when there is no object present in the system
     def no_obj_update(self, g):
-        m1 = self.max_angle[0]/140
-        m2 = self.max_angle[1]/100
-
+        m1 = self.max_angle[0]/140.0
+        m2 = self.max_angle[1]/100.0
+        
         theta1 = 0
         theta2 = 0
         theta3 = 0
-
+       
         if g <= 110:
             theta1 = m1*g
             theta2 = 0
             theta3 = -m1*g
+          
         elif g <= 140:
+            
             theta1 = m1*g
             theta2 = 0
             theta3 = self.min_3
         elif g <= 240:
+            print "3"
             theta1 = self.max_angle[0]
             theta2 = m2*(g - 140)
             theta3 = self.min_3
         else:
+            "4"
             theta1 = self.max_angle[0]
             theta2 = self.max_angle[1]
             theta3 = self.min_3
+ 
+        self.joints = [theta1, theta2, theta3]
 
-        self.joints = (theta1, theta2, theta3)
         return
 
     # def update_joints(self, g):
@@ -101,7 +108,27 @@ class Finger():
 
     #     self.update_status(deltaG)
     #     self.g = g
-    
+
+class Robotiq_gripper(object):
+    def __init__(self, robot):
+        self.gripper = robot
+        self.A = Finger()    
+        self.B = Finger()
+        self.C = Finger()
+
+    def update_fingers(self, status):
+        self.A.no_obj_update(status.gPOA)
+        self.B.no_obj_update(status.gPOB)
+        self.C.no_obj_update(status.gPOC)
+        self.gripper.SetDOFValues(list(itertools.chain(Deg2Rad(self.A.joints), 
+                                                       Deg2Rad(self.B.joints), 
+                                                       Deg2Rad(self.C.joints))))
+    def get_effector_pos(self):
+        manip = self.gripper.SetActiveManipulator("FingerA")
+        T = manip.GetEndEffectorTransform()
+        self.A.
+
+
 
 def gripper_model():
     env = openravepy.Environment()
@@ -111,10 +138,12 @@ def gripper_model():
     viewer.SetBkgndColor([.8, .85, .9])  # RGB tuple
     robot = env.GetRobots()[0]
 
-    T11 = robot.GetLinks()[7].GetTransform()
-    T12 = robot.GetLinks()[8].GetTransform()
+    gripper = Robotiq_gripper(robot)
+    while(1):
+        g = int(raw_input("set value of g: "))
+        test = Test(g)
+        gripper.update_fingers(test)
 
-    # gripper = Robotiq_gripper(robot)
     IPython.embed()
     # rospy.init_node('GripperDisplay')
     # rospy.Subscriber("/gripper_server/input", inputMsg.SModel_robot_input, gripper.update_fingers)
